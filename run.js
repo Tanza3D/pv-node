2908619e67ab076a2345035e9c0c4224b291aa48
Tanza3D <archie@untone.uk>
2025-03-07 21:21:20 +0000

    handle screen clear

--- run.js
+++ run.js
@@ -1,10 +1,10 @@
 var isPi = require('detect-rpi');
 var matrixLib = "./sim/rpi-led-matrix";
-if(isPi()) {
+if (isPi()) {
     matrixLib = "rpi-led-matrix";
 
-    const { execSync } = require("child_process");
-    const { argv, env } = require("process");
+    const {execSync} = require("child_process");
+    const {argv, env} = require("process");
 
     function isRunningAsRoot() {
         return process.getuid && process.getuid() === 0;
@@ -15,7 +15,7 @@ if(isPi()) {
         const command = `sudo ${process.execPath} ${argv.slice(1).join(" ")}`;
 
         try {
-            execSync(command, { stdio: "inherit", env });
+            execSync(command, {stdio: "inherit", env});
         } catch (error) {
             console.error("Failed to restart with sudo:", error);
             process.exit(1);
@@ -27,8 +27,8 @@ if(isPi()) {
     console.log("Running as root!");
 }
 
-const { LedMatrix, GpioMapping } = require(matrixLib);
-const { Jimp } = require('jimp');
+const {LedMatrix, GpioMapping} = require(matrixLib);
+const {Jimp} = require('jimp');
 const JimpR = require('jimp');
 
 const matrix = new LedMatrix({
@@ -42,6 +42,15 @@ const matrix = new LedMatrix({
     gpioSlowdown: 4
 }).clear();
 
+
+process.on('SIGINT', function () {
+    console.log("Cleaning screen properly :3");
+    matrix.fgColor({r: 1, g: 0, b: 0})
+    matrix.clear();
+    process.exit();
+});
+
+
 async function displayImage() {
     const image = await Jimp.read('v1.png');
     image.resize({w: 64, h: 32});
@@ -52,8 +61,8 @@ async function displayImage() {
     function drawImage(img, offsetX) {
         for (let y = 0; y < 32; y++) {
             for (let x = 0; x < 64; x++) {
-                const { r, g, b } = JimpR.intToRGBA(img.getPixelColor(x, y));
-                matrix.fgColor({ r, g, b }).setPixel(x + offsetX, y);
+                const {r, g, b} = JimpR.intToRGBA(img.getPixelColor(x, y));
+                matrix.fgColor({r, g, b}).setPixel(x + offsetX, y);
             }
         }
     }
